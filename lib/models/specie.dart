7b97fb72c035ca2e104bcb30a928fc87af255d96
Diogo Santos <dgssantos@hotmail.com>
2021-11-28 16:09:15 -0300

    feat: remove toJson methods

--- lib/models/specie.dart
+++ lib/models/specie.dart
@@ -51,18 +51,17 @@ class Specie {
   Specie.fromJson(Map<String, dynamic> json) {
     baseHappiness = json['base_happiness'];
     captureRate = json['capture_rate'];
-    color =
-        json['color'] != null ? new ObjPokemon.fromJson(json['color']) : null;
+    color = json['color'] != null ? ObjPokemon.fromJson(json['color']) : null;
     evolutionChain = json['evolution_chain'] != null
-        ? new EvolutionChain.fromJson(json['evolution_chain'])
+        ? EvolutionChain.fromJson(json['evolution_chain'])
         : null;
     evolvesFromSpecies = json['evolves_from_species'] != null
-        ? new ObjPokemon.fromJson(json['evolves_from_species'])
+        ? ObjPokemon.fromJson(json['evolves_from_species'])
         : null;
     if (json['flavor_text_entries'] != null) {
       flavorTextEntries = <FlavorTextEntries>[];
       json['flavor_text_entries'].forEach((v) {
-        flavorTextEntries!.add(new FlavorTextEntries.fromJson(v));
+        flavorTextEntries!.add(FlavorTextEntries.fromJson(v));
       });
     }
     formsSwitchable = json['forms_switchable'];
@@ -70,14 +69,14 @@ class Specie {
     if (json['genera'] != null) {
       genera = <Genera>[];
       json['genera'].forEach((v) {
-        genera!.add(new Genera.fromJson(v));
+        genera!.add(Genera.fromJson(v));
       });
     }
     generation = json['generation'] != null
-        ? new ObjPokemon.fromJson(json['generation'])
+        ? ObjPokemon.fromJson(json['generation'])
         : null;
     growthRate = json['growth_rate'] != null
-        ? new ObjPokemon.fromJson(json['growth_rate'])
+        ? ObjPokemon.fromJson(json['growth_rate'])
         : null;
     hasGenderDifferences = json['has_gender_differences'];
     hatchCounter = json['hatch_counter'];
@@ -87,85 +86,29 @@ class Specie {
     if (json['names'] != null) {
       names = <Names>[];
       json['names'].forEach((v) {
-        names!.add(new Names.fromJson(v));
+        names!.add(Names.fromJson(v));
       });
     }
     order = json['order'];
     if (json['pal_park_encounters'] != null) {
       palParkEncounters = <PalParkEncounters>[];
       json['pal_park_encounters'].forEach((v) {
-        palParkEncounters!.add(new PalParkEncounters.fromJson(v));
+        palParkEncounters!.add(PalParkEncounters.fromJson(v));
       });
     }
     if (json['pokedex_numbers'] != null) {
       pokedexNumbers = <PokedexNumbers>[];
       json['pokedex_numbers'].forEach((v) {
-        pokedexNumbers!.add(new PokedexNumbers.fromJson(v));
+        pokedexNumbers!.add(PokedexNumbers.fromJson(v));
       });
     }
-    shape =
-        json['shape'] != null ? new ObjPokemon.fromJson(json['shape']) : null;
+    shape = json['shape'] != null ? ObjPokemon.fromJson(json['shape']) : null;
     if (json['varieties'] != null) {
       varieties = <Varieties>[];
       json['varieties'].forEach((v) {
-        varieties!.add(new Varieties.fromJson(v));
+        varieties!.add(Varieties.fromJson(v));
       });
     }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['base_happiness'] = this.baseHappiness;
-    data['capture_rate'] = this.captureRate;
-    if (this.color != null) {
-      data['color'] = this.color!.toJson();
-    }
-    if (this.evolutionChain != null) {
-      data['evolution_chain'] = this.evolutionChain!.toJson();
-    }
-    data['evolves_from_species'] = this.evolvesFromSpecies;
-    if (this.flavorTextEntries != null) {
-      data['flavor_text_entries'] =
-          this.flavorTextEntries!.map((v) => v.toJson()).toList();
-    }
-    data['forms_switchable'] = this.formsSwitchable;
-    data['gender_rate'] = this.genderRate;
-    if (this.genera != null) {
-      data['genera'] = this.genera!.map((v) => v.toJson()).toList();
-    }
-    if (this.generation != null) {
-      data['generation'] = this.generation!.toJson();
-    }
-    if (this.growthRate != null) {
-      data['growth_rate'] = this.growthRate!.toJson();
-    }
-    if (this.habitat != null) {
-      data['habitat'] = this.habitat!.toJson();
-    }
-    data['has_gender_differences'] = this.hasGenderDifferences;
-    data['hatch_counter'] = this.hatchCounter;
-    data['id'] = this.id;
-    data['is_baby'] = this.isBaby;
-    data['name'] = this.name;
-    if (this.names != null) {
-      data['names'] = this.names!.map((v) => v.toJson()).toList();
-    }
-    data['order'] = this.order;
-    if (this.palParkEncounters != null) {
-      data['pal_park_encounters'] =
-          this.palParkEncounters!.map((v) => v.toJson()).toList();
-    }
-    if (this.pokedexNumbers != null) {
-      data['pokedex_numbers'] =
-          this.pokedexNumbers!.map((v) => v.toJson()).toList();
-    }
-    if (this.shape != null) {
-      data['shape'] = this.shape!.toJson();
-    }
-    if (this.varieties != null) {
-      data['varieties'] = this.varieties!.map((v) => v.toJson()).toList();
-    }
-    return data;
   }
 }
 
@@ -179,13 +122,6 @@ class ObjPokemon {
     name = json['name'];
     url = json['url'];
   }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['url'] = this.url;
-    return data;
-  }
 }
 
 class EvolutionChain {
@@ -198,7 +134,7 @@ class EvolutionChain {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+    final Map<String, dynamic> data = Map<String, dynamic>();
     data['url'] = this.url;
     return data;
   }
@@ -213,24 +149,10 @@ class FlavorTextEntries {
 
   FlavorTextEntries.fromJson(Map<String, dynamic> json) {
     flavorText = json['flavor_text'];
-    language = json['language'] != null
-        ? new ObjPokemon.fromJson(json['language'])
-        : null;
-    version = json['version'] != null
-        ? new ObjPokemon.fromJson(json['version'])
-        : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['flavor_text'] = this.flavorText;
-    if (this.language != null) {
-      data['language'] = this.language!.toJson();
-    }
-    if (this.version != null) {
-      data['version'] = this.version!.toJson();
-    }
-    return data;
+    language =
+        json['language'] != null ? ObjPokemon.fromJson(json['language']) : null;
+    version =
+        json['version'] != null ? ObjPokemon.fromJson(json['version']) : null;
   }
 }
 
@@ -242,18 +164,8 @@ class Genera {
 
   Genera.fromJson(Map<String, dynamic> json) {
     genus = json['genus'];
-    language = json['language'] != null
-        ? new ObjPokemon.fromJson(json['language'])
-        : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['genus'] = this.genus;
-    if (this.language != null) {
-      data['language'] = this.language!.toJson();
-    }
-    return data;
+    language =
+        json['language'] != null ? ObjPokemon.fromJson(json['language']) : null;
   }
 }
 
@@ -264,19 +176,9 @@ class Names {
   Names({this.language, this.name});
 
   Names.fromJson(Map<String, dynamic> json) {
-    language = json['language'] != null
-        ? new ObjPokemon.fromJson(json['language'])
-        : null;
+    language =
+        json['language'] != null ? ObjPokemon.fromJson(json['language']) : null;
     name = json['name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.language != null) {
-      data['language'] = this.language!.toJson();
-    }
-    data['name'] = this.name;
-    return data;
   }
 }
 
@@ -288,19 +190,9 @@ class PalParkEncounters {
   PalParkEncounters({this.area, this.baseScore, this.rate});
 
   PalParkEncounters.fromJson(Map<String, dynamic> json) {
-    area = json['area'] != null ? new ObjPokemon.fromJson(json['area']) : null;
+    area = json['area'] != null ? ObjPokemon.fromJson(json['area']) : null;
     baseScore = json['base_score'];
     rate = json['rate'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.area != null) {
-      data['area'] = this.area!.toJson();
-    }
-    data['base_score'] = this.baseScore;
-    data['rate'] = this.rate;
-    return data;
   }
 }
 
@@ -312,18 +204,8 @@ class PokedexNumbers {
 
   PokedexNumbers.fromJson(Map<String, dynamic> json) {
     entryNumber = json['entry_number'];
-    pokedex = json['pokedex'] != null
-        ? new ObjPokemon.fromJson(json['pokedex'])
-        : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['entry_number'] = this.entryNumber;
-    if (this.pokedex != null) {
-      data['pokedex'] = this.pokedex!.toJson();
-    }
-    return data;
+    pokedex =
+        json['pokedex'] != null ? ObjPokemon.fromJson(json['pokedex']) : null;
   }
 }
 
@@ -335,17 +217,7 @@ class Varieties {
 
   Varieties.fromJson(Map<String, dynamic> json) {
     isDefault = json['is_default'];
-    pokemon = json['pokemon'] != null
-        ? new ObjPokemon.fromJson(json['pokemon'])
-        : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['is_default'] = this.isDefault;
-    if (this.pokemon != null) {
-      data['pokemon'] = this.pokemon!.toJson();
-    }
-    return data;
+    pokemon =
+        json['pokemon'] != null ? ObjPokemon.fromJson(json['pokemon']) : null;
   }
 }
